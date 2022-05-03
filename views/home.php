657024bd9d558a6efc2d528eca54a8d54fb03c20
ScriptKiddie / <schnarfengargle123@gmail.com>
2022-05-04 00:34:33 +0100

    Created base layout for main content cards

--- views/home.php
+++ views/home.php
@@ -19,4 +19,87 @@
         </div>
     </div>
 
+    <main>
+        <div class="cards">
+            <div class="cards__panel">
+                <div class="cards__panel__section">
+                    <div class="cards__card">
+                        <img src="" alt="">
+                        <h4>MAP SEARCH</h4>
+                        <h4>Property Maps</h4>
+                        <strong>Quick links: </strong>
+                        <a>England</a>
+                        <a>Scotland</a>
+                        <a>Wales</a>
+                        <a>Northern Ireland</a>
+                        <a>London</a>
+                        <a>London Stations.</a>
+                        <hr>
+                        "or"
+                        <a href="#">Draw a search</a>
+                    </div>
+                    <div class="cards__card">
+                        <img src="" alt="">
+                        <h4>PROPERTY NEWS</h4>
+                        <a href="#">Dynamic Link</a>
+                        <p>Read more...</p>
+                        <hr>
+                        <a href="#">Visit the Shitemove Blog</a>
+                    </div>
+                    <div class="cards__card">
+                        <img src="" alt="">
+                        <h4>MOVING STORIES</h4>
+                        <a href="#">Dynamic Link</a>
+                        <p>Read "name's" story...</p>
+                        <hr>
+                        <a href="#">Find out more</a>
+                    </div>
+                </div>
+
+                <div class="cards__card--wide">
+                    <h3>Where can I live?</h3>
+                    <p>We've created a new tool to help you easily find the areas that match your budget and needs.</p>
+                    <a href="#">Discover your perfect location</a>
+                </div>
+
+                <div class="cards__panel__section">
+                    <div class="cards__blurb">
+                        <h4>Sold house prices</h4>
+                        <p>See what property in your local area sold for</p>
+                        <a href="#">Search Now</a>
+                    </div>
+                    <div class="cards__blurb">
+                        <h4>Commercial Property</h4>
+                        <p>Search freehold and leasehold commercial properties in the UK</p>
+                        <a href="#">Search Now</a>
+                    </div>
+                    <div class="cards__blurb">
+                        <h4>Investors & Landlords</h4>
+                        <p>Get the latest rent and yield figures direct to your inbox</p>
+                        <a href="#">Sign up now</a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="cards__panel">
+                <div class="cards__card--register">
+                    <img src="">
+                    <p>Create alerts and save properties & searches</p>
+                    <button>Create an account</button>
+                    <button>Sign in</button>
+                </div>
+                <div class="cards__card--recent">
+                    <h3>Recent searches</h3>
+                    <a href="#">Dynamic Link</a>
+                    <p>To Rent - Flats & Houses</p>
+                    <a href="#">Dynamic Link</a>
+                    <p>For Sale - Flats & Houses</p>
+                    <button><i>Icon</i>Clear Recent Searches</button>
+                </div>
+            </div>
+        </div>
+    </main>
+
+
 </div>
