892c851d53192577c64d08b06ad9446c39e6c581
ScriptKiddie / <schnarfengargle123@gmail.com>
2022-05-03 14:01:05 +0100

    Created search modal with styling

--- views/home.php
+++ views/home.php
@@ -1,1 +1,23 @@
-<h1>Home</h1>
+<div class="home">
+
+    <header>
+        <div class="header-content"></div>
+
+    </header>
+
+    <div class="search-box">
+        <div class="search-box__blurb">
+            <h1>Your dream home awaits</h1>
+            <p>Discover properties available to rent or purchase within the UK</p>
+        </div>
+
+        <div class="search-box__controls">
+            <input type="text" placeholder="e.g. 'London', 'SW1', 'SW1, TA6', or 'Westminster Station'">
+            <div class="search-box__controls__buttons">
+                <button class="search-box__controls__buttons__button" id="rent">To Rent</button>
+                <button class="search-box__controls__buttons__button" id="sale">For Sale</button>
+            </div>
+        </div>
+    </div>
+
+</div>
